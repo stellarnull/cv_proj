7f350ffddc09725e012596565b6b448247b4b911
stellarnull <cxystellar@foxmail.com>
2018-12-09 16:49:23 -0500

    test 1110

--- scripts/experiments/net.py
+++ scripts/experiments/net.py
@@ -302,7 +302,30 @@ class Vgg16(torch.nn.Module):
 
     #     return [relu1_1]
 
-    # 0100
+    # # 1100
+    # def forward(self, X):
+    #     h = F.relu(self.conv1_1(X))
+    #     relu1_1 = h
+    #     h = F.relu(self.conv1_2(h))
+    #     h = F.max_pool2d(h, kernel_size=2, stride=2)
+
+    #     h = F.relu(self.conv2_1(h))
+    #     relu2_1 = h
+    #     h = F.relu(self.conv2_2(h))
+    #     h = F.max_pool2d(h, kernel_size=2, stride=2)
+
+    #     h = F.relu(self.conv3_1(h))
+    #     h = F.relu(self.conv3_2(h))
+    #     h = F.relu(self.conv3_3(h))
+    #     h = F.max_pool2d(h, kernel_size=2, stride=2)
+
+    #     h = F.relu(self.conv4_1(h))
+    #     h = F.relu(self.conv4_2(h))
+    #     h = F.relu(self.conv4_3(h))
+
+    #     return [relu1_1, relu2_1]
+
+    # 1110
     def forward(self, X):
         h = F.relu(self.conv1_1(X))
         relu1_1 = h
@@ -315,6 +338,7 @@ class Vgg16(torch.nn.Module):
         h = F.max_pool2d(h, kernel_size=2, stride=2)
 
         h = F.relu(self.conv3_1(h))
+        relu3_1 = h
         h = F.relu(self.conv3_2(h))
         h = F.relu(self.conv3_3(h))
         h = F.max_pool2d(h, kernel_size=2, stride=2)
@@ -323,7 +347,7 @@ class Vgg16(torch.nn.Module):
         h = F.relu(self.conv4_2(h))
         h = F.relu(self.conv4_3(h))
 
-        return [relu1_1, relu2_1]
+        return [relu1_1, relu2_1, relu3_1]
 
 
 class Net(nn.Module):
